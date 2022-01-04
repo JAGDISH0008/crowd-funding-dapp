2f8edd5f249d0c8cd03c6a3fc2de2077173f0948
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-01-04 15:02:20 +0530

    dockarized smart contract.

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -6,6 +6,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "hardhat-watcher";
 
 dotenv.config();
 
@@ -38,6 +39,13 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
+  watcher: {
+    compilation: {
+      tasks: ["compile"],
+      files: ["./contracts"],
+      verbose: true,
+    }
+  }
 };
 
 export default config;
