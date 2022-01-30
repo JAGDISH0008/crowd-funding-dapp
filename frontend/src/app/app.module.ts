c466b72e15deb09152490b372e98332ac7d7bede
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-01-30 07:08:37 +0530

    header component moved to modules.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,18 @@
+import { HeaderModule } from './modules/header/header.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/modules/header/header.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [],
+  providers: [],
+  bootstrap: [],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class HeaderModule { }
